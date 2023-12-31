288b8e6b5d5b3138d3c01f01b901183c43684c12
Hmtgit7 <hmtloharcoding3579@gmail.com>
2023-12-31 19:23:39 +0530

    creating homepage

--- frontend/src/Pages/HomePage.js
+++ frontend/src/Pages/HomePage.js
@@ -1,11 +1,60 @@
-import React from 'react'
+import React from "react";
+import {
+  Container,
+  Box,
+  Text,
+  Tabs,
+  TabList,
+  TabPanels,
+  TabPanel,
+  Tab,
+} from "@chakra-ui/react";
+import SignUp from "../components/Authentication/SignUp";
+import Login from "../components/Authentication/Login";
 
 const HomePage = () => {
   return (
-    <div>
-      <h1>Home</h1>
-    </div>
-  )
-}
+    <Container maxW="xl" centerContent>
+      <Box
+        d="flex"
+        justifyContent="center"
+        p={3}
+        bg="white"
+        w="100%"
+        m="40px 0 15px 0"
+        borderRadius="lg"
+        borderWidth="1px"
+        // opacity="0.5"
+      >
+        <Text fontSize="4xl" fontFamily="Work sans" color="black">
+          MsgHub
+        </Text>
+      </Box>
+      <Box
+        w="100%"
+        p={4}
+        bg="white"
+        borderRadius="lg"
+        borderWidth="1px"
+        color="black"
+      >
+        <Tabs variant="soft-rounded">
+          <TabList mb="1rem">
+            <Tab width="50%">Login</Tab>
+            <Tab width="50%">SignUp</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <Login />
+            </TabPanel>
+            <TabPanel>
+              <SignUp />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </Box>
+    </Container>
+  );
+};
 
 export default HomePage;
