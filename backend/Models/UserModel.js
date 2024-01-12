0ddc46a85e1cd22320fc4f7f007c01f333b896ce
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-01-12 17:08:58 +0530

    modify

--- backend/Models/UserModel.js
+++ backend/Models/UserModel.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose')
 const UserModel = mongoose.Schema(
     {
         name: { type: String, required: true },
-        email: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
         password: { type: String, required: true },
-        picture: { type: String, required: true, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" },
+        picture: { type: String, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" },
 
     },{
         timestamps:true,
