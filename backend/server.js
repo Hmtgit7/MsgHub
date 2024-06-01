2130fd74168ec2f695810cbaea2bf14c240440c0
Hmtgit7 <hmtloharcoding3579@gmail.com>
2024-06-01 20:12:28 +0530

    adding error handler

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const dotenv=require("dotenv")
 const { chats } = require("./data/data");
 const connectDB =require('./config/db')
 const userRoutes=require('./routes/userRoutes.js')
+const {notFound,errorHandler}=require('./middleware/errorMiddleware')
 
 const app = express();
 dotenv.config();
@@ -25,6 +26,9 @@ app.get("/api/chat/:id", (req, res) => {
 });
 
 app.use('/api/user',userRoutes)
+
+app.use(notFound)
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000;
 
